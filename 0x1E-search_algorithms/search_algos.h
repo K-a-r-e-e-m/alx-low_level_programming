cb68ac0b75987578bfe5ba7109f5b0308c603fe1
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-18 02:14:17 +0300

    Add prototype of print_array function

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,8 @@
 #define _SEARCH_ALGOS_
 #include <stdio.h>
 
-
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+void print_array(int *array, size_t start, size_t end);
 
 #endif /*_SEARCH_ALGOS_*/
