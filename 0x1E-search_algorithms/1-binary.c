043245673d2d4bdc1bfcfcb54e017bbaaa308f73
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-07-18 02:13:15 +0300

    Add implementation of binary search algorithm implement binary search algorithm implement print_array function that print wehen array changes check for null value of array return the index of found value "

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Search for value in list of numbers
+ * @array: pointer to the first element of the array
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: Index of value or -1 when not founc or array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l, r;
+	int m;
+
+	if (!array)
+		return (-1);
+
+	/* l ==> left  ...  r ==> right ...  m ==> middle */
+	l = 0;
+	r = size - 1;
+	print_array(array, l, r);
+	while (l < r)
+	{
+		m = (l + r) / 2;
+		if (array[m] < value)
+		{
+			l = m + 1;
+			print_array(array, l, r);
+		}
+		else if (array[m] > value)
+		{
+			r = m  - 1;
+			print_array(array, l, r);
+		}
+		else
+			return (m);
+	}
+	return (-1);
+}
+
+/**
+ * print_array - print array every time it changes
+ * @array: pointer to the first element of the array
+ * @start: start index of array or subarray = l
+ * @end: last index of array or subarray = r
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = start; i <= end; i++)
+	{
+		if (i < end)
+			printf("%d, ", array[i]);
+		else
+			printf("%d", array[i]);
+	}
+	printf("\n");
+}
